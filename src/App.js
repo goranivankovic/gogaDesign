fb057db11776f894bcf3a691f837b2d00f88601c
nsCoder123 <goranivankovic87@gmail.com>
2022-02-09 19:53:35 +0100

    a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 
-import React, { Component } from 'react'
-import AllTogether from './AllTogether'
+
+
 import {BrowserRouter,Route, Switch } from 'react-router-dom';
+import { DiVim } from 'react-icons/di';
+import Navic from './components/Nav/Navic';
+import { Provider } from 'react-redux'
+import store from './redux/store'
 
-export default class App extends Component {
-  constructor(){
-    super()
-  }
-  render() {
-    return (
-      <BrowserRouter>
-      <div>
-        <Switch>
-          <Route exact path="/" component={AllTogether} />
-          <Route path='*' render={()=><h1>404 Page not found!!!</h1>} />
 
-         
 
-        </Switch>
-      </div>
-      </BrowserRouter>
-    )
-  }
+import React from 'react';
+import Home from './components/Home/Home';
+import Services from './components/Services/Services';
+import Tech from './components/Tech/Tech';
+import Projects from './components/Projects/Projects';
+import Price from './components/Prices/Price';
+import Contact from './components/Contact/Contact';
+
+
+
+
+
+
+export default function App() {
+
+
+  return <div>
+      <Provider store={store}>
+        <Navic />
+        <Home />
+        <Services />
+        <Tech />
+        <Projects />
+        <Price />
+        <Contact />
+
+      </Provider>
+
+
+  </div>;
 }
